bc68c5b9b2673170b7c4329179fb0d233be71474
mohammed ramadan <ramadanmohammed502@gmail.com>
2023-03-14 16:29:07 +0200

    install micro-cors

--- pages/api/product/index.jsx
+++ pages/api/product/index.jsx
@@ -1,5 +1,4 @@
 import nc from "next-connect";
-
 import dbConnect from "../../../utils/db/dbConnect";
 import { disconnect } from "../../../utils/db/dbConnect";
 
@@ -11,12 +10,12 @@ import mongoose from "mongoose";
 // import AllProducts from '../../../components/db'
 cloudinary.config({
   cloud_name: "dtmjc8y9z",
-  api_key: "379966828288349",
   api_secret: "a41LSvU3XXAJuQOLxorhOVFPauw",
 });
+import Cors from "micro-cors";
 
 const cors = Cors({
-  origin: "*",
+  // origin: "*",
   methods: ["GET", "HEAD", "POST", "PUT", "PATCH", "DELETE"],
 });
 
@@ -48,81 +47,81 @@ handler.get(async (req, res) => {
   // await disconnect();
 });
 
-handler.post(async (req, res) => {
-  const { db } = await dbConnect();
+// handler.post(async (req, res) => {
+//   const { db } = await dbConnect();
 
-  console.log("pData", req.body);
-  try {
-    let images = [];
+//   console.log("pData", req.body);
+//   try {
+//     let images = [];
 
-    if (typeof req.body.images === "string") {
-      images.push(req.body.images);
-    } else {
-      images = req.body.images;
-    }
+//     if (typeof req.body.images === "string") {
+//       images.push(req.body.images);
+//     } else {
+//       images = req.body.images;
+//     }
 
-    const imagesLinks = [];
+//     const imagesLinks = [];
 
-    for (let i = 0; i < images.length; i++) {
-      const result = await cloudinary.v2.uploader.upload(images[i], {
-        folder: "ecommerce",
-      });
+//     for (let i = 0; i < images.length; i++) {
+//       const result = await cloudinary.v2.uploader.upload(images[i], {
+//         folder: "ecommerce",
+//       });
 
-      imagesLinks.push({
-        public_id: result.public_id,
-        url: result.secure_url,
-      });
-    }
+//       imagesLinks.push({
+//         public_id: result.public_id,
+//         url: result.secure_url,
+//       });
+//     }
 
-    req.body.images = imagesLinks;
-    // req.body.user = req.user.id;
-    console.log(req.body);
-    const product = await Product.create(req.body);
+//     req.body.images = imagesLinks;
+//     // req.body.user = req.user.id;
+//     console.log(req.body);
+//     const product = await Product.create(req.body);
 
-    res.status(201).json({
-      success: true,
-      product,
-    });
-  } catch (err) {
-    res.status(404).json({
-      message: err.message,
-    });
-  }
-  // await db.disconnect();
-});
+//     res.status(201).json({
+//       success: true,
+//       product,
+//     });
+//   } catch (err) {
+//     res.status(404).json({
+//       message: err.message,
+//     });
+//   }
+//   // await db.disconnect();
+// });
 
-// create product --> admin
-handler.put(async (req, res) => {
-  // try
-  // {
-  const { db } = await dbConnect();
-  console.log(req.body);
-  const resPerPage = req.body.n ? req.body.n : 0;
+// // create product --> admin
+// handler.put(async (req, res) => {
+//   // try
+//   // {
+//   const { db } = await dbConnect();
+//   console.log(req.body);
+//   const resPerPage = req.body.n ? req.body.n : 0;
 
-  const productsCount = await Product.countDocuments();
-  const apiFeatures = new APIFeatures(Product.find(), req.query)
-    .search()
-    .filter();
-  let products = await apiFeatures.query;
-  let filteredProductsCount = products.length;
+//   const productsCount = await Product.countDocuments();
+//   const apiFeatures = new APIFeatures(Product.find(), req.query)
+//     .search()
+//     .filter();
+//   let products = await apiFeatures.query;
+//   let filteredProductsCount = products.length;
 
-  apiFeatures.pagination(products.length);
-  products = await apiFeatures.query.clone();
-  const { query, paginationResult } = apiFeatures;
+//   apiFeatures.pagination(products.length);
+//   products = await apiFeatures.query.clone();
+//   const { query, paginationResult } = apiFeatures;
 
-  res.status(200).json({
-    success: true,
-    paginationResult,
-    // resPerPage,
-    products,
-    filteredProductsCount,
-    productsCount,
-  });
-  // } catch (err)
-  // {
-  //   return res.status(500).json({ message: err.message });
-  // }
-  // await db.disconnect();
-});
+//   res.status(200).json({
+//     success: true,
+//     paginationResult,
+//     // resPerPage,
+//     products,
+//     filteredProductsCount,
+//     productsCount,
+//   });
+//   // } catch (err)
+//   // {
+//   //   return res.status(500).json({ message: err.message });
+//   // }
+//   // await db.disconnect();
+// });
 
 export default handler;
