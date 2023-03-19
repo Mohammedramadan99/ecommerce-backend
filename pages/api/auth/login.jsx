c7e5de29af91a87cd61c4e0c17f02a16f829e60f
mohammed ramadan <ramadanmohammed502@gmail.com>
2023-03-19 14:15:54 +0200

    Update login middleware

--- pages/api/auth/login.jsx
+++ pages/api/auth/login.jsx
@@ -17,8 +17,12 @@ const handler = nc();
 handler.post(async (req, res) => {
   // Set the CORS headers
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST,PUT, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", true);
 
   await cors(req, res);
 
