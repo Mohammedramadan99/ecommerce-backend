18d5c84f7d09d6b7605533073abe340d4d3f7b0e
mohammed ramadan <ramadanmohammed502@gmail.com>
2023-03-20 01:13:25 +0200

    Update review middleware

--- utils/auth.jsx
+++ utils/auth.jsx
@@ -3,9 +3,9 @@ import User from "../Modal/userModel";
 import dbConnect from "../utils/db/dbConnect";
 
 export const isAuth = async (req, res, next) => {
+  const { db } = await dbConnect();
   try {
     console.log("headers:", req.headers);
-    const { db } = await dbConnect();
     let token = req.headers.authorization.split(" ")[1];
 
     if (!token) {
@@ -17,13 +17,16 @@ export const isAuth = async (req, res, next) => {
     }
 
     const verified = jwt.verify(token, process.env.JWT_KEY);
+    console.log("verified", verified);
+
     const user = await User.findById(verified?.id).select("-password");
-    if (!user) {
-      return res.status(403).send("login first");
-    }
     console.log("user", user);
-    req.user = user;
-    next();
+    if (!user) {
+      return res.status(401).json("login first");
+    } else {
+      req.user = user;
+      next();
+    }
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
